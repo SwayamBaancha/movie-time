606a81c785d913d27ec22e79e1b53c819aa4145a
Swayam <mern1.baancha@gmail.com>
2024-06-12 16:46:49 +0530

    comment bug fix

--- src/app/movie-detail/[detail]/page.tsx
+++ src/app/movie-detail/[detail]/page.tsx
@@ -10,7 +10,7 @@ import Slider from "@/components/Carousel";
 
 export default async function Page({ params }: { params: { detail: string } }) {
   const movieDetail = await fetchMovieDetail(params.detail);
-  // const comment = await fetchCommentFromDb(params.detail);
+  const comment = await fetchCommentFromDb(params.detail);
   
   return (
     <div>
@@ -79,7 +79,7 @@ export default async function Page({ params }: { params: { detail: string } }) {
             })}
           </div> */}
           <div className="flex items-center justify-between mt-4">
-              {/* <CommentForm movie_id={params.detail} refresh={movieDetail} /> */}
+              <CommentForm movie_id={params.detail} refresh={movieDetail} />
             <Button variant="ghost">
               <Link
                 href={movieDetail?.homepage || ""}
@@ -93,9 +93,9 @@ export default async function Page({ params }: { params: { detail: string } }) {
         </div>
       </div>
       
-      {/* <div>
+      <div>
         <h3 className="font-bold text-lg">Reviews</h3>
-        {comment.length>1 ? comment.map((item: any, index: any) => {
+        {comment.length>=1 ? comment.map((item: any, index: any) => {
           return (
             <div
               key={index}
@@ -107,7 +107,7 @@ export default async function Page({ params }: { params: { detail: string } }) {
             </div>
           );
         }) : <p className="text-white text-sm mt-4">No review yet. Be the first one to review</p>}
-      </div> */}
+      </div>
         {/* <Slider upcomingMovies={similarMovies} className="w-full max-w-xs" isAutoplay={false} /> */}
     </div>
   );
